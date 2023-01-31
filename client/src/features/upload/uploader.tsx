c78869efa95d1f17486bdef8245a867b4ac80945
lekyvong09 <lekyvong09@gmail.com>
2023-01-31 20:05:56 +0700

    react router

---
+++ client/src/features/upload/uploader.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Uploader() {
+    return (
+        <Typography variant="h2">
+            Uploader
+        </Typography>
+    );
+}
--- client/src/layout/App.tsx
+++ client/src/layout/App.tsx
@@ -4,6 +4,11 @@ import Catalog from '../features/catalog/Catalog';
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import Header from './Header';
 import { Container } from '@mui/system';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../features/home/HomePage';
+import AboutPage from '../features/about/AboutPage';
+import ContactPage from '../features/contact/ContactPage';
+import Uploader from '../features/upload/uploader';
 
 
 function App() {
@@ -20,7 +25,15 @@ function App() {
         <CssBaseline />
         <Header onSetDarkMode={setDarkMode} darkMode={darkMode} />
         <Container>
-          <Catalog/>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='catalog' element={<Catalog/>} />
+            <Route path='catalog/:productId' element={<Catalog/>} />
+            <Route path='about' element={<AboutPage />} />
+            <Route path='contact' element={<ContactPage />} />
+            <Route path='upload' element={<Uploader />} />
+          </Routes>
+          
         </Container>
       </ThemeProvider>
       
