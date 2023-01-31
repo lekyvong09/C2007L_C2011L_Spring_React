ad28b784ea93fe20c3a76765f2f25e54d93d9419
lekyvong09 <lekyvong09@gmail.com>
2023-01-31 21:25:25 +0700

    upload image

--- client/src/features/upload/uploader.tsx
+++ client/src/features/upload/uploader.tsx
@@ -1,9 +1,28 @@
-import { Typography } from "@mui/material";
+import { Input, Typography } from "@mui/material";
 
 export default function Uploader() {
+    const onFileChangeHandler = (e: any) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('file', e.target.files[0]);
+        fetch('http://localhost:8080/api/file/upload', {
+            method: 'post',
+            body: formData
+        }).then(res => {
+            if (res.ok) {
+                alert("File uploaded successfully");
+            }
+        });
+        console.log(formData);
+    };
+
     return (
-        <Typography variant="h2">
-            Uploader
-        </Typography>
+        <>
+            <Typography variant="h2">
+                Uploader
+            </Typography>
+
+            <Input type="file" onChange={onFileChangeHandler}/>
+        </>
     );
 }
