2762a6fa85d68372a3819ea8c9f72008b306f5d7
lekyvong09 <lekyvong09@gmail.com>
2023-02-11 20:13:06 +0700

    API: remove basket item

--- api/src/main/java/com/ray/api/controller/BasketController.java
+++ api/src/main/java/com/ray/api/controller/BasketController.java
@@ -1,10 +1,12 @@
 package com.ray.api.controller;
 
+import com.ray.api.dao.BasketItemRepository;
 import com.ray.api.dao.BasketRepository;
 import com.ray.api.dao.ProductRepository;
 import com.ray.api.dto.BasketDto;
 import com.ray.api.dto.BasketItemDto;
 import com.ray.api.entity.Basket;
+import com.ray.api.entity.BasketItem;
 import com.ray.api.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,11 +26,13 @@ import java.util.stream.Collectors;
 public class BasketController {
     private final BasketRepository basketRepository;
     private final ProductRepository productRepository;
+    private final BasketItemRepository basketItemRepository;
 
     @Autowired
-    public BasketController(BasketRepository basketRepository, ProductRepository productRepository) {
+    public BasketController(BasketRepository basketRepository, ProductRepository productRepository, BasketItemRepository basketItemRepository) {
         this.basketRepository = basketRepository;
         this.productRepository = productRepository;
+        this.basketItemRepository = basketItemRepository;
     }
 
     @GetMapping
@@ -82,6 +86,51 @@ public class BasketController {
         basket.addItem(product, quantity);
         Basket returnBasket = basketRepository.save(basket);
 
+        /**
+         * Transform to basketDTO
+         */
+        List<BasketItemDto> basketItemDtoList = returnBasket.getBasketItems().stream().map(item -> new BasketItemDto(
+                item.getProduct().getId(),
+                item.getProduct().getName(),
+                item.getProduct().getUnitPrice(),
+                item.getProduct().getImageUrl(),
+                item.getProduct().getBrand(),
+                item.getProduct().getCategory().getCategoryName(),
+                item.getQuantity()
+        )).collect(Collectors.toList());
+
+        BasketDto basketDto = new BasketDto();
+        basketDto.setId(returnBasket.getId());
+        basketDto.setBuyerId(returnBasket.getBuyerId());
+        basketDto.setBasketItems(basketItemDtoList);
+
+        return new ResponseEntity<>(basketDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<BasketDto> removeBasketItem(@RequestParam("productId") Long productId,
+                                                      @RequestParam("quantity") int quantity,
+                                                      @CookieValue(name="buyerId", defaultValue = "") String buyerId) {
+        List<Basket> basketList = basketRepository.findByBuyerId(buyerId);
+
+        if (basketList.isEmpty())
+            throw new NoResultException("Cannot find the basket");
+
+        Basket basket = basketList.get(0);
+        BasketItem existingItem = basket.getBasketItems().stream()
+                .filter(i -> i.getProduct().getId().equals(productId))
+                .findAny().orElse(null);
+        if (existingItem == null)
+            throw new NoResultException("The basket doesn't have this item");
+
+        int newQuantity = existingItem.getQuantity() - quantity;
+
+        if (newQuantity <= 0) {
+            basket.getBasketItems().remove(existingItem);
+            basketItemRepository.delete(existingItem);
+        }
+
+        Basket returnBasket = basketRepository.save(basket);
         /**
          * Transform to basketDTO
          */
