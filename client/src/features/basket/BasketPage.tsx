417af01861ac944cbcf36ac98bb1c939fdef8b9f
lekyvong09 <lekyvong09@gmail.com>
2023-02-21 20:44:00 +0700

    react: add & remove item out of basket

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,11 +1,30 @@
-import { Delete } from "@mui/icons-material";
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import { useContext } from "react";
+import { AddCircle, Delete, RemoveCircle } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import axios, { AxiosResponse } from "axios";
+import { useContext, useState } from "react";
 import { StoreContext } from "../../context/StoreContext";
 
 
 export default function BasketPage() {
-    const {basket} = useContext(StoreContext);
+    const {basket, setBasket, removeItem} = useContext(StoreContext);
+    const [loading, setLoading] = useState(false);
+
+    const handleAddItem = (productId: number) => {
+        setLoading(true);
+        axios.post(`baskets?productId=${productId}&quantity=1`, {})
+            .then((response: AxiosResponse) => setBasket(response.data))
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false));
+    }
+
+    const handleRemoveItem = (productId: number, quantity: number) => {
+        setLoading(true);
+        axios.delete(`baskets?productId=${productId}&quantity=${quantity}`)
+            .then(() => removeItem(productId, quantity))
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false));
+    }
 
     if (!basket)
         return <Typography variant="h3">Basket is empty</Typography>
@@ -17,7 +36,7 @@ export default function BasketPage() {
                     <TableRow>
                         <TableCell>Product</TableCell>
                         <TableCell align="right">Price</TableCell>
-                        <TableCell align="right">Quantity</TableCell>
+                        <TableCell align="center">Quantity</TableCell>
                         <TableCell align="right">Subtotal</TableCell>
                         <TableCell align="right"></TableCell>
                     </TableRow>
@@ -32,12 +51,33 @@ export default function BasketPage() {
                             {row.name}
                         </TableCell>
                         <TableCell align="right">${row.unitPrice}</TableCell>
-                        <TableCell align="right">{row.quantity}</TableCell>
+
+                        <TableCell align="center">
+                            <LoadingButton 
+                                color="secondary"
+                                loading={loading}
+                                onClick={() => handleAddItem(row.productId)}
+                            >
+                                <AddCircle />
+                            </LoadingButton>
+                                {row.quantity}
+                            <LoadingButton 
+                                color="error"
+                                loading={loading}
+                                onClick={() => handleRemoveItem(row.productId, 1)}
+                            >
+                                <RemoveCircle />
+                            </LoadingButton>
+                        </TableCell>
+
                         <TableCell align="right">${(row.unitPrice * row.quantity).toFixed(2)}</TableCell>
                         <TableCell align="right">
-                            <IconButton color="error">
+                            <LoadingButton 
+                                color="error"
+                                onClick={() => handleRemoveItem(row.productId, row.quantity)}
+                            >
                                 <Delete />
-                            </IconButton>
+                            </LoadingButton>
                         </TableCell>
                         </TableRow>
                     ))}
